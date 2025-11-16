6a3804cd04de1f9539c44374802780a636adfd34
Taji Abdullah <tajiabdullah1@gmail.com>
2025-11-16 09:40:37 -0500

    Update build to use Github secrets.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     splunk = {
-        source = "splunk/splunk"
-        version = ">=1.0"
+      source  = "splunk/splunk"
+      version = ">=1.0"
     }
   }
 }
 
 provider "splunk" {
-  url = var.splunk_url
-  insecure_skip_verify = true
-  username = var.splunk_username
-  password = var.splunk_password
+  url                  = var.splunk_url
+  insecure_skip_verify = var.insecure_skip_verify
+  username             = var.splunk_username
+  password             = var.splunk_password
 }
