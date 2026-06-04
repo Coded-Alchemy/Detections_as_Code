1650072b51545a43d6625ca2a9be4bfa91dfe145
Taji Abdullah <tajiabdullah1@gmail.com>
2026-06-04 19:53:07 -0400

    Add macro. (#39)
    * Add macro.
    
    * Remove provider
    
    * Fix revert versioning.
    
    ---------
    
    Co-authored-by: Taji Abdullah <codedalchemy@gmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,15 +3,6 @@
 # ============================================
 
 
-terraform {
-  required_providers {
-    splunk = {
-      source  = "splunk/splunk"
-      version = ">=1.0"
-    }
-  }
-}
-
 provider "splunk" {
   url                  = var.splunk_url
   insecure_skip_verify = var.insecure_skip_verify
